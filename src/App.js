d95896824aae7daf3256b26a66f714e7db3fad0f
Mohammad Raadi <raadi@bitsnbytes.ir>
2019-02-04 16:12:00 +0330

    added react-helmet to the mix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { IntlProvider } from 'react-intl';
 import { FormattedMessage } from 'react-intl';
+import { Helmet } from 'react-helmet';
 import * as actions from './redux/appStore/actions';
 import messages from './i18n/messages';
 
 class App extends Component {
   componentDidMount(){
-    // const { changeLocale } = this.props;
-    // changeLocale('fa');
+    const { changeLocale } = this.props;
+    changeLocale('fa');
   }
   render() {
     const { selectedLocale } = this.props;
@@ -18,9 +19,17 @@ class App extends Component {
         locale={selectedLocale.locale}
         messages={messages[selectedLocale.locale]}
         >
-        <h1>
-          <FormattedMessage id="app.header"/>
-        </h1>
+        <Fragment>
+          <Helmet
+            encodeSpecialCharacters={true}  
+            >
+            <html lang={selectedLocale.locale} />
+            <body dir={selectedLocale.direction} />
+          </Helmet>
+          <h1>
+            <FormattedMessage id="app.header"/>
+          </h1>
+        </Fragment>
       </IntlProvider>
     );
   }
