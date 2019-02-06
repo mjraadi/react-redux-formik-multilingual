235ee66f899e3df70a59fed1ef17ca11e1530ecb
Mohammad Raadi <raadi@bitsnbytes.ir>
2019-02-06 13:47:39 +0330

    Updates

---
+++ precache-manifest.de0e2dc7ad09898bbd171982c35b0f54.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "647a439a9426201d5c86",
+    "url": "/react-redux-formik-multilingual/static/js/runtime~main.647a439a.js"
+  },
+  {
+    "revision": "bdf7fa8635ba3f17c089",
+    "url": "/react-redux-formik-multilingual/static/js/main.bdf7fa86.chunk.js"
+  },
+  {
+    "revision": "1c57e70e2ad5effb8179",
+    "url": "/react-redux-formik-multilingual/static/js/1.1c57e70e.chunk.js"
+  },
+  {
+    "revision": "30e9be141f42af61de62fe85e90f2856",
+    "url": "/react-redux-formik-multilingual/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-redux-formik-multilingual/precache-manifest.de0e2dc7ad09898bbd171982c35b0f54.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-redux-formik-multilingual/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
